777bcfb1069069c7a05e08a90fd01105070cfb3a
wasiur32 <121707101+wasiur32@users.noreply.github.com>
2024-04-05 08:57:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,33 @@
+
+
 from flask import Flask, render_template, request, redirect, flash
 from werkzeug.utils import secure_filename
 from main import getPrediction
 import os
 
-UPLOAD_FOLDER = 'static/image/'
+#Save images to the 'static' folder as Flask serves images from this directory
+UPLOAD_FOLDER = 'static/images/'
+
+#Create an app object using the Flask class. 
 app = Flask(__name__, static_folder="static")
+
+#Add reference fingerprint. 
+#Cookies travel with a signature that they claim to be legit. 
+#Legitimacy here means that the signature was issued by the owner of the cookie.
+#Others cannot change this cookie as it needs the secret key. 
+#It's used as the key to encrypt the session - which can be stored in a cookie.
+#Cookies should be encrypted if they contain potentially sensitive information.
 app.secret_key = "secret key"
+
+#Define the upload folder to save images uploaded by the user. 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+#Add Post method to the decorator to allow for form submission. 
 @app.route('/', methods=['POST'])
 def submit_file():
     if request.method == 'POST':
@@ -23,14 +39,15 @@ def submit_file():
             flash('No file selected for uploading')
             return redirect(request.url)
         if file:
-            try:
-            	filename = secure_filename(file.filename)
-            	file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            	label = getPrediction(filename)
-            	full_filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            	return render_template('result.html', label=label, image=full_filename)
-            except:
-                return render_template('result_no.html')
+            filename = secure_filename(file.filename)  #Use this werkzeug method to secure filename. 
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            #getPrediction(filename)
+            label = getPrediction(filename)
+            flash(label)
+            full_filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            flash(full_filename)
+            return redirect('/')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
